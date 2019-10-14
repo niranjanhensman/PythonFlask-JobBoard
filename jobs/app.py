584ff38ffd5b2ddc20ba671ffa14b0db60016e6e
niranjanhensman <nhensman11@gmail.com>
2019-10-14 11:23:35 +1100

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -15,7 +15,7 @@ def open_connection():
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
-    if commit ==True:
+    if commit == True:
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
@@ -32,4 +32,5 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)
